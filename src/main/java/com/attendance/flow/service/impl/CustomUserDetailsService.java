a7c14b8bdea2f10828bc1646e52de45dec4ffb49
RostyslavBehei <begejrostyk@gmail.com>
2026-03-31 18:10:10 +0300

    feat: implement CustomUserDetailsService for database authentication

---
+++ src/main/java/com/attendance/flow/service/impl/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.attendance.flow.service.impl;
+
+import com.attendance.flow.model.User;
+import com.attendance.flow.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " not found!"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                Collections.singleton(new SimpleGrantedAuthority(user.getRole().name()))
+        );
+    }
+}
